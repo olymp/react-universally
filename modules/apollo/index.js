c6d8b3e358e993cd3aa567292d2eced1cc36532c
Benjamin Kniffler <bkniffler@me.com>
2017-02-08 11:15:00 +0100

    Remove modules, add devDeps to deps

--- modules/apollo/index.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-module.exports = (config) => {
-  config.bundles.client.srcEntryFile = path.resolve(__dirname, 'universally', 'client.js');
-  config.bundles.client.srcPaths.push(path.resolve(__dirname, 'universally'));
-  config.bundles.server.srcEntryFile = path.resolve(__dirname, 'universally', 'server.js');
-  config.bundles.server.srcPaths.push(path.resolve(__dirname, 'universally'));
-  return config;
-};
